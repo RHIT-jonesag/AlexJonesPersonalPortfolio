02e0f2ea230e6c07658db01112bd8d4964e0c3fe
RHIT-jonesag <107145709+RHIT-jonesag@users.noreply.github.com>
2023-06-30 14:34:23 -0400

    Got login button working

---
+++ script.js
@@ -0,0 +1,11 @@
+// Let Functions
+
+// Function for login button
+function results() {
+    // let functions for the function
+    let loginScreen = document.querySelector(".log-in-screen");
+    let homeScreen = document.querySelector(".home-screen");
+
+    loginScreen.style.display="none";
+    homeScreen.style.display = "flex";
+}
