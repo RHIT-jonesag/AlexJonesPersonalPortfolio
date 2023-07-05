2ca82780ce9c4d3fec644a7348f36566a05db607
RHIT-jonesag <107145709+RHIT-jonesag@users.noreply.github.com>
2023-07-05 09:26:25 -0400

    Fixed stuff (finally)

--- script.js
+++ script.js
@@ -36,14 +36,3 @@ $(function() {
             typeWriterPassword();
         }, 1700)
 });
-
-
-// Function for login button
-function results() {
-    // let functions for the function
-    let loginScreen = document.querySelector(".log-in-screen");
-    let homeScreen = document.querySelector(".home-screen");
-
-    loginScreen.style.display="none";
-    homeScreen.style.display = "flex";
-}
